762995656653ac2f694f5712f562b39546db4f68
Gustavo Torres N <gtn.280693@gmail.com>
2022-11-19 14:31:21 -0500

    repair bugs

--- js/change-img-nav.js
+++ js/change-img-nav.js
@@ -3,11 +3,14 @@ function ChangeNavImage(){
     const navBarCartoon = document.querySelector("#nav-bar__cartoon")
     const width = screen.width
     const height = screen.height
-    if(width < 900 && height < width ){
+    if(height < width ){
+        console.log('landscape')  
         navBarCartoon.src = "/img/pointer-right.png"
     }else{
+        console.log('portraid')  
         navBarCartoon.src = "/img/pointer-top.png"
-    }   
+    } 
+    
 }
 
 export default ChangeNavImage
--- js/index.js
+++ js/index.js
@@ -91,3 +91,42 @@ function autoType(elementClass, typingSpeed, typeOut){
      }, 5000);
  }
 
+// download CV
+
+$(function(){
+  var flag=0;
+  
+  $('.share').on('click',function(){
+   if(flag == 0)
+    {
+      $(this).siblings('.one').animate({
+      bottom:'100px',
+      left:'48',
+    },200);
+    
+    //  $(this).siblings('.two').delay(200).animate({
+    //   top:'260px',
+    //   left:'40%'
+    // },200);
+    
+    //  $(this).siblings('.three').delay(300).animate({
+    //   top:'260px',
+    //   left:'60%'
+    // },200);
+              
+    $('.one i,.two i, .three i').delay(500).fadeIn(200);  
+      flag = 1;
+    }
+    
+    
+  else{
+    $('.one, .two, .three').animate({
+        bottom:'30',
+        left:'48px'
+      },200);
+      
+  $('.one i,.two i, .three i').delay(500).fadeOut(200);
+      flag = 0;
+    }
+  });
+});
