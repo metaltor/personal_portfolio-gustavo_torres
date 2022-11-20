9f58ac21ab5f4ae60d6437871074e4e0f6613519
Gustavo Torres N <gtn.280693@gmail.com>
2022-11-20 00:18:36 -0500

    variable order change and dark theme structure

--- js/index.js
+++ js/index.js
@@ -131,3 +131,22 @@ $(function(){
   });
 });
 
+//change background
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch-wrapper input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  let lightThemeText = document.getElementById("light-theme-text");
+  let darkThemeText = document.getElementById("dark-theme-text");
+  lightThemeText.classList.toggle("disabled");
+  darkThemeText.classList.toggle("disabled");
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
